8a2c5d6fad46fbcaf4a93fb853eeec111d3963f9
msolorio <msolorio789@gmail.com>
2017-12-13 22:25:50 -0800

    add promise to http request

--- app.js
+++ app.js
@@ -2,19 +2,30 @@ const request = require('request');
 const { address } = require('./args');
 
 function getCoordinates(addr) {
-  request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}`,
-    json: true,
-    method: 'GET'
-  }, (error, response, body) => {
-    if (error) {
-      return console.log('error:', error);
-    }
+  return new Promise((resolve, reject) => {
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}`,
+      json: true,
+      method: 'GET'
+    }, (error, response, body) => {
 
-    console.log('address:', body.results[0].formatted_address);
-    console.log('latitude:', body.results[0].geometry.location.lat);
-    console.log('longitute:', body.results[0].geometry.location.lng);
+      if (error) reject(error);
+
+      const result = (
+`address: ${body.results[0].formatted_address}
+latitude: ${body.results[0].geometry.location.lat}
+longitute: ${body.results[0].geometry.location.lng}`
+      );
+
+      resolve(result);
+    });
   });
 }
 
-getCoordinates(address);
+getCoordinates(address)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error('error:', error);
+  });
